1cbd27b33ad2756dc3dee8a0ec1120fc2c3c5feb
Majisto <marciors92@gmail.com>
2026-02-11 19:57:04 -0300

    feat: estrutura layout principal e integra componentes ao TodoProvider

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <div className="app-container">
       <header>
-        <h1>Minha Lista de Afazeres</h1>
+        <h1>Lista de Afazeres</h1>
         <TodoForm />
       </header>
 
@@ -27,7 +27,9 @@ export default function App() {
             />
           ))}
         </ul>
-        {filteredTodos.length === 0 && <p className="empty">Nenhuma tarefa encontrada.</p>}
+        {filteredTodos.length === 0 && (
+          <p className="empty-state">Por ora, nenhuma tarefa... ☕</p>
+        )}
       </main>
     </div>
   );
