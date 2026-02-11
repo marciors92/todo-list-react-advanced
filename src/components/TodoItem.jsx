8d4a20fadd099c7d9910a5b61d482c9daf3217ef
Majisto <marciors92@gmail.com>
2026-02-11 19:10:33 -0300

    perf: cria componente TodoItem utilizando React.memo

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// React.memo impede que o item renderize se não houver mudança nele
+export const TodoItem = React.memo(({ todo, onToggle, onDelete }) => {
+    return (
+        <li className={`todo-item ${todo.completed ? 'completed' : ''}`}>
+            <span onClick={() => onToggle(todo.id)}>{todo.text}</span>
+            <button onClick={() => onDelete(todo.id)}>Remover</button>
+        </li>
+    );
+});
